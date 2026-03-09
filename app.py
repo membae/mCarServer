4cbcdd8b62c51152b01baed6dfc3641aa370e271
membae <membaphilip1@gmail.com>
2026-03-09 17:54:00 +0300

    app.py

--- app.py
+++ app.py
@@ -1,2 +1,46 @@
-from models import User,Mechanic,Garage,Car,Review,Service,Sparepart
+from models import db,User,Mechanic,Garage,Car,Review,Service,Sparepart
 from flask_migrate import Migrate
+from flask import Flask, request, make_response,jsonify
+from flask_restful import Api, Resource
+from flask_jwt_extended import JWTManager,create_access_token, create_refresh_token,jwt_required,get_jwt_identity
+import secrets,datetime,os
+from datetime import timedelta
+from flask_cors import CORS
+from werkzeug.utils import secure_filename
+from werkzeug.security import check_password_hash,generate_password_hash
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DATABASE = os.environ.get(
+    "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
+   
+UPLOAD_FOLDER = os.path.join(os.getcwd(), 'static')
+ALLOWED_EXTENSIONS=set(['png','jpeg','jpg'])
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit(".",1)[1].lower() in ALLOWED_EXTENSIONS
+
+app = Flask(__name__)
+CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] =secrets.token_hex(32)
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=30)
+app.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(days=30)
+app.config['UPLOAD_FOLDER']=UPLOAD_FOLDER
+
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+api=Api(app)
+jwt=JWTManager(app)
+
+
+class Home(Resource):
+    def get(self):
+        return make_response({"msg":"Cars Homepage here"},200)
+
+api.add_resource(Home,'/')
+
+
+if __name__=="__main__":
+    app.run(debug=True)
